f06df694f0d025831a8e65553068a6fdf1dd6c86
Marko Ristin <marko.ristin@gmail.com>
2020-04-17 10:38:02 +0200

    Infer translation language from the browser (#33)
    Before this commit, the default language was just picked as first in the list of available translations. With this commit the default language is first inferred from the browser first.

---
+++ src/__tests__/i18n.test.ts
@@ -0,0 +1,19 @@
+import * as i18n from "../i18n";
+
+it("selects the first language when no language matches.", () => {
+  const lang = i18n.inferDefault("en-GB", ["hr", "sr"]);
+
+  expect(lang).toBe("hr");
+});
+
+it("selects the exact match if available.", () => {
+  const lang = i18n.inferDefault("en", ["hr", "en", "sr"]);
+
+  expect(lang).toBe("en");
+});
+
+it("selects the primary language match if available.", () => {
+  const lang = i18n.inferDefault("en-GB", ["hr", "en", "sr"]);
+
+  expect(lang).toBe("en");
+});
