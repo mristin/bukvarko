1b72cb7eab3092f1add9937c29c856540f81d0f5
Marko Ristin <marko.ristin@gmail.com>
2020-04-22 21:15:03 +0200

    Make stars and speaker larger on desktop (#61)

--- src/components/ScoreBar.tsx
+++ src/components/ScoreBar.tsx
@@ -10,10 +10,11 @@ import * as app from '../app';
 import * as question from '../question';
 import * as select from '../select';
 
-function Indicator(props: { hit: boolean; current: boolean }) {
+function Indicator(props: { hit: boolean; current: boolean; large?: boolean }) {
   const style = {
     color: props.hit ? yellow[700] : grey[100],
     ...(props.current ? { background: '#6495ED', borderRadius: '50%' } : {}),
+    ...(props.large ? { fontSize: '3em' } : {}),
   };
 
   return (
@@ -25,17 +26,17 @@ function Indicator(props: { hit: boolean; current: boolean }) {
   );
 }
 
-function Score(props: { hitsIDs: Array<[boolean, question.ID]>; currentIndex: number }) {
+function Score(props: { hitsIDs: Array<[boolean, question.ID]>; currentIndex: number; large?: boolean }) {
   return (
     <>
       {props.hitsIDs.map(([hit, id], i) => {
-        return <Indicator key={id} hit={hit} current={props.currentIndex === i} />;
+        return <Indicator key={id} hit={hit} current={props.currentIndex === i} large={props.large} />;
       })}
     </>
   );
 }
 
-export function ScoreBar() {
+export function ScoreBar(props: { large?: boolean }) {
   const selectContext = useContext(select.Context);
   if (selectContext === undefined) {
     throw Error('Expected selector context to be set.');
@@ -44,5 +45,5 @@ export function ScoreBar() {
   const hitsIDs = useSelector((s: app.State) => selectContext.hitsIDs(s));
   const currentIndex = useSelector((s: app.State) => selectContext.currentIndex(s));
 
-  return <Score hitsIDs={hitsIDs} currentIndex={currentIndex} />;
+  return <Score hitsIDs={hitsIDs} currentIndex={currentIndex} large={props.large} />;
 }
